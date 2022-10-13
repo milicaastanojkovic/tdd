eb44e7e062ea85d4c6e470c630ddc2b8a1876d60
milicaastanojkovic <77588733+milicaastanojkovic@users.noreply.github.com>
2022-10-13 12:03:41 +0200

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,29 +1,36 @@
-  //undefined if a == undefiend 
-  
   function subCalculator (a, b) {
     return a - b;
   }
 
-  function addCalculator(a, b) {
-    try 
-    {
-      if(a === undefined || b === undefined)
-          throw "not enough parametars";
-      else return a + b;
-    }
-    catch(error)
-    {
-      console.log(error);
-      throw error;
-    }
-  }  
+  // function addCalculator(a, b) {
+  //   try 
+  //   {
+  //     if(a === undefined || b === undefined)
+  //         throw "not enough parametars";
+  //     else return a + b;
+  //   }
+  //   catch(error)
+  //   {
+  //     console.log(error);
+  //     throw error;
+  //   }
+  // }  
+
+  function addCalculator(a,b){
+    if(arguments.length < 2) 
+      throw "not enough parametars";
+    else return a + b;
+  }
 
   function multiCalculator (a, b) {
-    return a * b;
+    if (typeof a === 'string' || a instanceof String || typeof b === 'string' || b instanceof String) 
+      throw "parametars can't be strings";
+    else return a * b;
   }
 
   function divCalculator (a, b) {
-    if(b=0) throw new Error( 'Divide by zero.' ); 
+    if(b==0) 
+      throw "Divide by zero."; 
     else return a / b;
   }
 
@@ -32,5 +39,36 @@
     return numbers.reduce((sum,value) => sum+value, 0)
   }
   
-  module.exports = {subCalculator, multiCalculator, addCalculator, addMore, divCalculator}
+//4. zahtev - Function takes an array of numbers, analyze it, and return object 
+//that has information about number of elements, average value of elements, maximum and minimum value. 
+//Implement handling error that function throw if one of elements is string, or undefined.
+
+ //var array = [1,2,'22',33,44,123,1976];
+
+  function avgArray(array){
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum/array.length;
+  }
+
+  function analyzeArr(array){
+    var obj = {
+      total: array.length,
+      avg: avgArray(array),
+      max: Math.max.apply(Math, array),
+      min: Math.min.apply(Math, array)  
+    }
+
+    for (let i = 0; i < array.length; i++) {
+      if(array[i] === 'string' || array[i] instanceof String) throw "Array elements can't be strings"
+      if(array[i] === undefined) throw "Array elements can't be undefined"
+      else return obj;
+    }
+  }
+
+
+  //module.exports = {subCalculator, multiCalculator, addCalculator, addMore, divCalculator}
+  module.exports = {analyzeArr}
 
--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,6 @@
-const{subCalculator, multiCalculator, addMore, divCalculator, addCalculator} = require("./calculator");
-
+//const{subCalculator, multiCalculator, addMore, divCalculator, addCalculator} = require("./calculator");
+const{analyzeArr} = require("./calculator");
+/*
 test("substraction of 3 and 2 to equal 1", () => {
   expect(subCalculator(3, 2)).toBe(1);
 });
@@ -8,28 +9,36 @@ test("multiplication of 3 and 2 to equal 6", () => {
   expect(multiCalculator(3, 2)).toBe(6);
 });
 
+test("multiplication when one of the parametars is string", () => {
+  expect(() => multiCalculator(3, 'a')).toThrow("parametars can't be strings");
+});
+
+test("multiplication when both of the parametars are strings", () => {
+  expect(() => multiCalculator('asc', 'a')).toThrow("parametars can't be strings");
+});
+
 test("addition of 2 and 3 and 1 to equal 6", () => {
    expect(addMore(2, 3, 1)).toBe(6);
  });
-
  
 test("division of 6 and 2 to equal 3", () => {
-   expect(divCalculator(6, 0)).toBe(Infinity);
+   expect(() => divCalculator(6, 0)).toThrow();
  });
-
-
-// test("not enough parametars", () => {
-//   expect(addCalculator(6)).toThrow("Not enough arguments; two expected");
-// });  
-
 
 test("not enough parametars", () => {
   expect(addCalculator(6,2)).toBe(8);
 });  
 
-
 test("not enough parametars", () => {
-  expect(addCalculator(6)).toThrow(TypeError);
+  expect(() => addCalculator(6)).toThrow("not enough parametars");
 });  
+*/
 
- 
+
+
+test("array consists of numbers and strings", () => {
+  let arr = [1,2,3,'123',1234,1,0,451,886]
+  //expect(() => addCalculator(6)).toThrow("not enough parametars");
+  expect(() => analyzeArr(arr)).toThrow("Array elements can't be strings");
+});
+
